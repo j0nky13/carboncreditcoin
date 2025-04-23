b5a8bae9832b8c8746bd6bc1c76a9640351642be
Jonathan <jonkyle@me.com>
2025-04-22 20:44:55 -0400

    refactor firebase.js to use env variables

--- client/src/lib/firebase.js
+++ client/src/lib/firebase.js
@@ -46,11 +46,9 @@
 
 // // Export auth for use in hooks and components
 // export { auth };
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
-// Firebase config using environment variables
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
@@ -58,7 +56,7 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
